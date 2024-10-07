7b92eaf2dc4609e17188098c41158ac8829d3974
Phil Austin <phil_a10@hotmail.com>
2024-10-07 22:16:03 +0100

    added conda demo sql connection not working new conda slides

---
+++ Data Engineering Using Free Tools/conda.ps1
@@ -0,0 +1,32 @@
+# create a new conda environment called myenv
+conda create --name myenv
+
+# create a new conda environment called myenv using python 3.9
+conda create --name myenv2 python=3.9 
+
+# install a package in the new environment
+conda install azure-identity
+
+# can also use Pip to install packages
+pip install azure-identity
+
+# activate the new environment
+conda activate myenv2
+
+# check conda environments
+# note that the active environment is marked with an asterisk (*)
+conda env list
+
+python --version
+
+# switch environments in VS Code using the Command Palette CTRL+SHIFT+P
+# relaunch the terminal to see the new environment
+
+conda env list
+
+python --version
+
+conda list
+
+# many other options are available - clone environments, remove environments, etc.
+# cheat sheet: https://docs.conda.io/projects/conda/en/latest/user-guide/cheatsheet.html
