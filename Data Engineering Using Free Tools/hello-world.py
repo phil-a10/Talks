2fac609fe5b705a7103f3bbb05ae08fa24127355
Phil Austin <phil_a10@hotmail.com>
2024-10-08 19:04:40 +0100

    added quickm python demo with simple viz

--- Data Engineering Using Free Tools/hello-world.py
+++ Data Engineering Using Free Tools/hello-world.py
@@ -1,1 +1,17 @@
 print('hello world')
+
+# list of tuples
+organisers = [
+        ('Dominic', 'Windsor'),
+        ('James', 'Yarrow'),
+        ('Anna-Marie', 'Wykes'),
+        ('Niall', 'Langley')
+        ]
+
+# import tabulate
+# this will need to be installed using pip ie pip install tabulate
+from tabulate import tabulate
+
+table = tabulate(organisers, headers=['First name', 'Last name'])
+
+print(table)
